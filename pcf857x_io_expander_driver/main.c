05a3336269a4558be09807508397001ed02426b3
LibraryMasters <cedricmalyam@hotmail.com>
2024-03-22 09:04:29 +0200

    updated example main file

--- pcf857x_io_expander_driver/main.c
+++ pcf857x_io_expander_driver/main.c
@@ -1,5 +1,16 @@
 #include "pcf857x_driver_basic.h"
 
+/**
+ * @brief application example state machine
+ */
+typedef enum
+{
+	READ_WRITE = 0x00,
+	TOGGLE     = 0x01,
+	WRITE_ALL  = 0x03
+
+}example_state_t;
+
 int main()
 {
 
@@ -20,6 +31,52 @@ int main()
     pcf857x_interface_debug_print("Diver Version: \t\tV%.1f.%.2d\r\n",(pcf857x_handler.info.driver_version /1000), (uint8_t)(pcf857x_handler.info.driver_version - (uint8_t)(pcf857x_handler.info.driver_version / 100)*100));
     pcf857x_interface_debug_print("\nRecommended min MCU flash: \t%dkB\r\n",pcf857x_handler.info.flash_size_min);
     pcf857x_interface_debug_print("Recommended min MCU RAM: \t%dkB\r\n",pcf857x_handler.info.ram_size_min);
+
+    example_state_t  example_state;
+
+     /**< uncomment one of below options to test the driver */
+     //example_state = READ_WRITE;
+     example_state = TOGGLE;
+     //example_state = WRITE_ALL;
+
+    while (1) {
+
+		 switch(example_state)
+		 {
+
+			 case READ_WRITE:
+			 {
+				 if(pcf857x_basic_gpio_read(PCF857X_GPIO_PIN_0) == LOW){           /**< read pin 0 and de-bounce button */
+					 pcf857x_interface_delay_ms(10);
+					 if(pcf857x_basic_gpio_read(PCF857X_GPIO_PIN_0) == LOW)
+					 {
+						 pcf857x_basic_gpio_write(PCF857X_GPIO_PIN_11, HIGH);
+					 }
+
+					 }else{
+					 pcf857x_basic_gpio_write(PCF857X_GPIO_PIN_11, LOW);
+				 }
+				 break;
+			 }
+
+			 case TOGGLE:
+			 {
+				 pcf857x_basic_gpio_toggle(PCF857X_GPIO_PIN_7);               /**< toggle pin 7 every 500 ms */
+				 pcf857x_interface_delay_ms(500);
+				 break;
+			 }
+
+			 case WRITE_ALL:
+			 {
+				 pcf857x_basic_gpio_write_all(HIGH);               /**< Write high on all port */
+				 pcf857x_interface_delay_ms(500);
+				 pcf857x_basic_gpio_write_all(LOW);                /**< low high on all port */
+				 pcf857x_interface_delay_ms(500);
+				 break;
+			 }
+
+		 }
+	}
 
     return 0;
 }
